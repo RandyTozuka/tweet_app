58165304903d198c0c5148a4f7591694d9154a20
Randy Tozuka <kazm1208@me.com>
2021-02-07 14:25:41 +0900

    モデルにhas many, belongs toを挿入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tweets
+  has_many :likes
   validates :text, length: { maximum: 100 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
